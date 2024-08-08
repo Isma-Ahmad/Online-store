61f9f0607edff1aa387dc9ac1c7c700b699fb00b
ismaahmadmad <ismaa7990@gmail.com>
2024-08-08 16:36:34 +0500

    online store

--- services/productService.js
+++ services/productService.js
@@ -2,42 +2,36 @@ const db = require('../models');
 
 
 const createProduct = async (productData, userId) => {
-    try {
+  
         const user = await db.User.findByPk(userId);
         if (user.userType !== 'admin') {
             throw new Error('Only admins can add products');
         }
         const product = await db.Product.create(productData);
         return product;
-    } catch (error) {
-        throw new Error('Error creating product: ' + error.message);
-    }
+   
 };
 
 
 const getAllProducts = async () => {
-    try {
+ 
         const products = await db.Product.findAll();
         return products;
-    } catch (error) {
-        throw new Error('Error retrieving products: ' + error.message);
-    }
+    
 };
 
 
 const getProductById = async (productId) => {
-    try {
+   
         const product = await db.Product.findByPk(productId);
         if (!product) throw new Error('Product not found');
         return product;
-    } catch (error) {
-        throw new Error('Error retrieving product: ' + error.message);
-    }
+   
 };
 
 
 const updateProduct = async (productId, updatedData, userId) => {
-    try {
+  
         const user = await db.User.findByPk(userId);
         if (user.userType !== 'admin') {
             throw new Error('Only admins can update products');
@@ -45,14 +39,12 @@ const updateProduct = async (productId, updatedData, userId) => {
         const product = await db.Product.findByPk(productId);
         if (!product) throw new Error('Product not found');
         return await product.update(updatedData);
-    } catch (error) {
-        throw new Error('Error updating product: ' + error.message);
-    }
+   
 };
 
 
 const deleteProduct = async (productId, userId) => {
-    try {
+
         const user = await db.User.findByPk(userId);
         if (user.userType !== 'admin') {
             throw new Error('Only admins can delete products');
@@ -61,9 +53,7 @@ const deleteProduct = async (productId, userId) => {
         if (!product) throw new Error('Product not found');
         await product.destroy();
         return { message: 'Product deleted successfully' };
-    } catch (error) {
-        throw new Error('Error deleting product: ' + error.message);
-    }
+    
 };
 
 module.exports = {
