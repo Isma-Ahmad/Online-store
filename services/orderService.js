66b41e6e79bf30904f936998bf9afe0d0d7da270
ismaahmadmad <ismaa7990@gmail.com>
2024-08-08 16:38:03 +0500

    online store

--- services/orderService.js
+++ services/orderService.js
@@ -2,17 +2,15 @@ const db = require('../models');
 
 
 const createOrder = async (orderData, userId) => {
-    try {
+   
         const order = await db.Order.create({ ...orderData, user_id: userId });
         return order;
-    } catch (error) {
-        throw new Error('Error creating order: ' + error.message);
-    }
+    
 };
 
 
 const getAllOrders = async (userId, userType) => {
-    try {
+  
         if (userType === 'admin') {
             const orders = await db.Order.findAll();
             return orders;
@@ -20,14 +18,12 @@ const getAllOrders = async (userId, userType) => {
             const orders = await db.Order.findAll({ where: { user_id: userId } });
             return orders;
         }
-    } catch (error) {
-        throw new Error('Error retrieving orders: ' + error.message);
-    }
+    
 };
 
 
 const getOrderById = async (orderId, userId, userType) => {
-    try {
+   
         const order = await db.Order.findByPk(orderId);
         if (!order) throw new Error('Order not found');
 
@@ -36,28 +32,24 @@ const getOrderById = async (orderId, userId, userType) => {
         }
 
         return order;
-    } catch (error) {
-        throw new Error('Error retrieving order: ' + error.message);
-    }
+   
 };
 
 
 const updateOrder = async (orderId, updatedData, userType) => {
-    try {
+  
         if (userType !== 'admin') {
             throw new Error('Only admins can update orders');
         }
         const order = await db.Order.findByPk(orderId);
         if (!order) throw new Error('Order not found');
         return await order.update(updatedData);
-    } catch (error) {
-        throw new Error('Error updating order: ' + error.message);
-    }
+   
 };
 
 
 const deleteOrder = async (orderId, userType) => {
-    try {
+  
         if (userType !== 'admin') {
             throw new Error('Only admins can delete orders');
         }
@@ -65,9 +57,7 @@ const deleteOrder = async (orderId, userType) => {
         if (!order) throw new Error('Order not found');
         await order.destroy();
         return { message: 'Order deleted successfully' };
-    } catch (error) {
-        throw new Error('Error deleting order: ' + error.message);
-    }
+    
 };
 
 module.exports = {
